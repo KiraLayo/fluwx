ab1a213cde8b2aae42810fc23b3455e837cdd812
qizheng.wen <qizheng.wen@yeepay.com>
2020-09-17 11:13:50 +0800

    no message

--- android/src/main/kotlin/com/jarvan/fluwx/handlers/FluwxResponseHandler.kt
+++ android/src/main/kotlin/com/jarvan/fluwx/handlers/FluwxResponseHandler.kt
@@ -52,8 +52,6 @@ object FluwxResponseHandler {
     private fun handleChooseInvoiceResponse(response: ChooseCardFromWXCardPackage.Resp) {
         val result = mapOf(
             "cardItemList" to response.cardItemList,
-            // "errStr" to response.errStr,
-            // "errCode" to response.errCode,
             type to response.type)
 
         channel?.invokeMethod("onChooseInvoiceResponse", result)
